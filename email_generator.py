8cc44fd0312243de9c9e376131ddb266ee0a8a06
trentz <mateus.trentz@gmail.com>
2020-02-05 10:24:48 -0300

    Removed some comments

--- email_generator.py
+++ email_generator.py
@@ -67,8 +67,7 @@ def gen_mail(to_mail, excel_file, owner, years=None, months=None, days=None, own
     newsletter.send_mail(to_mail, f'{best}', f'{worst}')
 
 
-gen_mail('mateus.trentz@gmail.com', 'M_info.xls', 'M', months=4, detail=True)
-# gen_mail('kochhann@anfip.org.br', 'C_info.xls', 'C', months=3, detail=True)
+gen_mail('ADD_TO_MAIL', 'M_info.xls', 'M', months=4, detail=True)
 
 # todo criar meu rcparam pra mais facil editar e mudar todos os graficos
 # todo lucro em reais nao aparece se o periodo é menor que o mês
